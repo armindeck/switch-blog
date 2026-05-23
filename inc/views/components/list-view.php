8c93bdf86ce78e7c1af84115e46520c1648be3ce
armindeck <dbhsoficial2003@gmail.com>
2026-05-22 21:24:43 -0500

    Optimización de las columnas y mejor orden del contenido

--- inc/views/components/list-view.php
+++ inc/views/components/list-view.php
@@ -3,13 +3,11 @@
     <table>
         <thead>
             <tr>
+                <td title="<?= language("stars") ?>">📊</td>
                 <td><?= language("title") ?></td>
                 <td><?= language("episode") ?></td>
-                <td><?= language("episodes") ?></td>
-                <td><?= language("season") ?></td>
                 <td><?= language("state") ?></td>
                 <td><?= language("type") ?></td>
-                <td><?= language("stars") ?></td>
                 <?php if(!$user || $user && $is_user_user): ?>
                 <td><?= language("action") ?></td>
                 <?php endif; ?>
@@ -18,13 +16,11 @@
         <tbody>
             <?php $i = 1; foreach ($list_order_by_state as $key => $value): ?>
             <tr <?= $i % 2 == 0 ? "style='background:rgb(0,0,0,.1);'" : ""  ?>>
+                <td><?= !empty($value["stars"]) ? starsEmoji($value["stars"]) : "" ?></td>
                 <td><?= !empty($value["url"]) ? "<a title=\"" . language("external_disclaimer") . "\" target=\"_blank\" href=\"{$value['url']}\" rel=\"noopener noreferrer\">" : "" ?><?= $value["title"] ?? "" ?><?= !empty($value["url"]) ? "</a>" : "" ?></td>
-                <td><?= $value["episode"] ?? "" ?></td>
-                <td><?= $value["episodes"] ?? "" ?></td>
-                <td><?= $value["season"] ?? "" ?></td>
-                <td><?= language($value["state"] ?? "") ?></td>
+                <td title="<?= language("season") . ' - ' . language("episode") . ' / ' . language("episodes") ?>"><?= (!empty($value["season"]) ? "T" . $value["season"] . " - " : "") . (!empty($value["episode"]) ? $value["episode"] : "") . (!empty($value["episode"]) && !empty($value["episodes"]) ? "/" . $value["episodes"] : "") ?></td>
+                <td title="<?= language($value["state"] ?? "") ?>"><?= stateEmoji($value["state"] ?? "") ?></td>
                 <td><?= language($value["type"] ?? "") ?></td>
-                <td><?= !empty($value["stars"]) ? $value["stars"] . " / 5" : "" ?></td>
                 <?php if(!$user || $user && $is_user_user): ?>
                     <td class="flex flex-between gap-4">
                         <a href="?id=<?= $key ?? "" ?>&action=edit<?= $user ? "&to_user=true" : "" ?>">📝</a>
