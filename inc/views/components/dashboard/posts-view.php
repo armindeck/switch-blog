891f63c6233b80c30a2dcdbe3b70a8e9534f371d
armindeck <dbhsoficial2003@gmail.com>
2026-07-01 02:33:50 -0500

    Mejore el diseño y corregi el error en las url

--- inc/views/components/dashboard/posts-view.php
+++ inc/views/components/dashboard/posts-view.php
@@ -27,13 +27,27 @@ $actions = ["new", "edit", "delete"];
 
 ?>
 <?php if(empty($get_action) || !in_array($get_action, $actions)): ?>
-<ul>
-<?php foreach ($blog_data as $key => $value): ?>
-    <li>
-        <a href="posts/edit/<?= $value['slug'] ?>"><?= $value['slug'] ?></a>
-    </li>
-<?php endforeach; ?>
-</ul>
+    <div class="content">
+    <div style="display: flex; flex-wrap: wrap; justify-content: space-between; gap: 4px">
+        <a href="../dashboard" style="background: var(--back-primary); color: var(--text-co); padding: 8px 15px; border: 1px solid var(--input-border-co); border-radius: 8px; font-weight: bold; cursor: pointer; transition: all 0.3s ease; font-size: 16px;">
+            ← <?= language("back") ?>
+        </a>
+        <a href="posts/new" style="background: var(--success-bg); color: var(--success-co); padding: 8px 15px; border: none; border-radius: 8px; font-weight: bold; cursor: pointer; transition: all 0.3s ease; font-size: 16px;">
+            ✍️ <?= language("new_post") ?>
+        </a>
+    </div>
+    <ul style="display: flex; flex-direction: column; gap: 4px;">
+    <?php foreach ($blog_data as $key => $value): ?>
+        <li style="display: flex; align-items: center; justify-content: space-between; gap: 4px; border: 1px solid rgba(0,0,0,.1); border-radius: 4px; padding: 4px 6px;">
+            <a href="posts/edit/<?= $value['slug'] ?>">✍️ <?= $value['slug'] ?></a>
+            <small>
+                <a href="<?= route("blog/{$value['slug']}") ?>" target="_blank" style="border: 1px solid rgba(0,0,0,.1); padding: 2px 4px; border-radius: 4px;"><?= $value["views"] ?? "0" ?> 👀</a>
+                <a style="border: 1px solid rgba(0,0,0,.1); padding: 2px 4px; border-radius: 4px;"><?= $value["likes"] ?? "0" ?> 👍</a>
+            </small>
+        </li>
+    <?php endforeach; ?>
+    </ul>
+    </div>
 <?php endif;
 
 if(in_array($get_action, $actions)){
