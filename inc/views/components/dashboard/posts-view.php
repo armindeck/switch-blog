d4100cd4b9fe9be5369571d09b260ab65793cd52
armindeck <dbhsoficial2003@gmail.com>
2026-07-05 23:13:16 -0500

    Mejore el diseño de la lista de posts, ahora se puede publicar un nuevo posts, editar posts, ver el numero de vistas y los me gustas, pronto estaran los borradores

--- inc/views/components/dashboard/posts-view.php
+++ inc/views/components/dashboard/posts-view.php
@@ -24,29 +24,46 @@ SOFTWARE.
 */
 
 $actions = ["new", "edit", "delete"];
-
+echo $view_hero_two(
+    language($get_section),
+    !empty($get_action) && in_array($get_action, $actions) ? route("dashboard/posts") : route("dashboard")
+);
 ?>
 <?php if(empty($get_action) || !in_array($get_action, $actions)): ?>
-    <div class="content">
-    <div style="display: flex; flex-wrap: wrap; justify-content: space-between; gap: 4px">
-        <a href="../dashboard" style="background: var(--back-primary); color: var(--text-co); padding: 8px 15px; border: 1px solid var(--input-border-co); border-radius: 8px; font-weight: bold; cursor: pointer; transition: all 0.3s ease; font-size: 16px;">
-            ← <?= language("back") ?>
-        </a>
-        <a href="posts/new" style="background: var(--success-bg); color: var(--success-co); padding: 8px 15px; border: none; border-radius: 8px; font-weight: bold; cursor: pointer; transition: all 0.3s ease; font-size: 16px;">
-            ✍️ <?= language("new_post") ?>
-        </a>
-    </div>
-    <ul style="display: flex; flex-direction: column; gap: 4px;">
-    <?php foreach ($blog_data as $key => $value): ?>
-        <li style="display: flex; align-items: center; justify-content: space-between; gap: 4px; border: 1px solid rgba(0,0,0,.1); border-radius: 4px; padding: 4px 6px;">
-            <a href="posts/edit/<?= $value['slug'] ?>">✍️ <?= $value['slug'] ?></a>
-            <small>
-                <a href="<?= route("blog/{$value['slug']}") ?>" target="_blank" style="border: 1px solid rgba(0,0,0,.1); padding: 2px 4px; border-radius: 4px;"><?= $value["views"] ?? "0" ?> 👀</a>
-                <a style="border: 1px solid rgba(0,0,0,.1); padding: 2px 4px; border-radius: 4px;"><?= $value["likes"] ?? "0" ?> 👍</a>
-            </small>
-        </li>
-    <?php endforeach; ?>
-    </ul>
+    <div class="content" style="margin: 0;">
+        <div style="display: flex; flex-direction: column; border: 1px solid var(--content-border-strong-co); border-radius: 4px; margin-top: 6px; overflow: hidden;">
+            <hgroup style="display: flex; align-items: center; justify-content: space-between; gap: 4px; border-bottom: 1px solid var(--content-border-strong-co); padding: 4px 6px;">
+                <strong><?= language("posts") ?></strong>
+                <a href="posts/new" style="background: var(--success-bg); color: var(--success-co); padding: 4px 6px; border-radius: 4px; font-size: 16px; font-weight: bold;">
+                    📝 <?= language("new_post") ?>
+                </a>
+            </hgroup>
+            <?php $i = 1; foreach ($blog_data ?? [] as $key => $value): ?>
+                <hgroup style="display: flex; align-items: center; justify-content: space-between; gap: 4px; <?= $i < count($blog_data) ? "border-bottom: 1px solid var(--content-border-co);" : "" ?> padding: 4px 6px;">
+                    <a href="posts/edit/<?= $value['slug'] ?>">✍️ <?= $value['slug'] ?></a>
+                    <small>
+                        <a href="<?= route("blog/{$value['slug']}") ?>" target="_blank" style="border: 1px solid var(--content-border-strong-co); padding: 2px 4px; border-radius: 4px;"><?= $value["views"] ?? "0" ?> 👀</a>
+                        <a style="border: 1px solid var(--content-border-strong-co); padding: 2px 4px; border-radius: 4px;"><?= $value["likes"] ?? "0" ?> 👍</a>
+                    </small>
+                </hgroup>
+            <?php $i++; endforeach; ?>
+            <?= empty($blog_data) ? "<small style='padding: 4px 6px;'>" . language("there_is_no_article") . "</small>" : "" ?>
+        </div>
+        <div style="display: flex; flex-direction: column; border: 1px solid var(--content-border-strong-co); border-radius: 4px; margin-top: 6px; overflow: hidden;">
+            <hgroup style="display: flex; align-items: center; justify-content: space-between; gap: 4px; border-bottom: 1px solid var(--content-border-strong-co); padding: 4px 6px;">
+                <strong><?= language("drafts") ?></strong>
+            </hgroup>
+            <?php $i = 1; foreach ($blog_data_drafts ?? [] as $key => $value): ?>
+                <hgroup style="display: flex; align-items: center; justify-content: space-between; gap: 4px; <?= $i < count($blog_data) ? "border-bottom: 1px solid var(--content-border-co);" : "" ?> padding: 4px 6px;">
+                    <a href="posts/edit/<?= $value['slug'] ?>">✍️ <?= $value['slug'] ?></a>
+                    <small>
+                        <a href="<?= route("blog/{$value['slug']}") ?>" target="_blank" style="border: 1px solid var(--content-border-strong-co); padding: 2px 4px; border-radius: 4px;"><?= $value["views"] ?? "0" ?> 👀</a>
+                        <a style="border: 1px solid var(--content-border-strong-co); padding: 2px 4px; border-radius: 4px;"><?= $value["likes"] ?? "0" ?> 👍</a>
+                    </small>
+                </hgroup>
+            <?php $i++; endforeach; ?>
+            <?= empty($blog_data_drafts) ? "<small style='padding: 4px 6px;'>" . language("there_is_no_draft") . "</small>" : "" ?>
+        </div>
     </div>
 <?php endif;
 
