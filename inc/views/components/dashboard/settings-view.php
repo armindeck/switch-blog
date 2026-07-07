bfeb1c898ce195d5d7faa37034ed1248e6bc2780
armindeck <dbhsoficial2003@gmail.com>
2026-07-07 00:11:57 -0500

    Removi el Markdown ya que no se usa aquí

--- inc/views/components/dashboard/settings-view.php
+++ inc/views/components/dashboard/settings-view.php
@@ -23,9 +23,6 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-use Michelf\Markdown;
-use Michelf\MarkdownExtra;
-
 echo $view_hero_two(
     language($get_section),
     route("dashboard")
